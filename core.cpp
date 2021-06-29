47f849be97a2ae9b95051c6a75b1d0ae37ac9c04
s.kolesnik <7fosGbc9S>
2021-06-29 17:33:08 +0500

    added tests with exports as a standalone shared library

--- core.cpp
+++ core.cpp
@@ -1,37 +1,9 @@
 ﻿
-#include "core_impl.ipp"
+#ifndef STANDALONE_EXPORTS
+#include "exports.ipp"
+#else
 #include "core.hpp"
 #include "export_types.hpp"
-
-template struct foo_template<old>;
-template int foo::get<old>();
-template int get<old>();
-
-#ifdef NEXT_VERSION
-
-struct new_0 {
-};
-template struct foo_template<new_0>;
-template int foo::get<new_0>();
-template int get<new_0>();
-
-struct new_1 {
-};
-template struct foo_template<new_1>;
-template int foo::get<new_1>();
-template int get<new_1>();
-
-struct new_2 {
-};
-template struct foo_template<new_2>;
-template int foo::get<new_2>();
-template int get<new_2>();
-
-struct new_3 {
-};
-template struct foo_template<new_3>;
-template int foo::get<new_3>();
-template int get<new_3>();
 #endif
 
 #include <dynalo/dynalo.hpp>
---
+++ exports.ipp
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+#include "core_impl.ipp"
+#include "export_types.hpp"
+
+template struct foo_template<old>;
+template int foo::get<old>();
+template int get<old>();
+
+#ifdef NEXT_VERSION
+
+struct new_0 {
+};
+template struct foo_template<new_0>;
+template int foo::get<new_0>();
+template int get<new_0>();
+
+struct new_1 {
+};
+template struct foo_template<new_1>;
+template int foo::get<new_1>();
+template int get<new_1>();
+
+struct new_2 {
+};
+template struct foo_template<new_2>;
+template int foo::get<new_2>();
+template int get<new_2>();
+
+struct new_3 {
+};
+template struct foo_template<new_3>;
+template int foo::get<new_3>();
+template int get<new_3>();
+#endif
